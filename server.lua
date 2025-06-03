a86dc48a0660113db01a33b4093832b9a0a44fbe
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2025-06-02 20:58:57 -0400

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,9 +1,9 @@
-RegisterNetEvent("phone:youtube_music:soundStatus", function(type, data)
+RegisterNetEvent("phone:utune_music:soundStatus", function(type, data)
     local src = source
-    local musicId = "phone_youtubemusic_id_" ..src
+    local musicId = "phone_utuneemusic_id_" ..src
     if type ~= "position" and type ~= "play" and type ~= "volume" and type ~= "stop" then 
-        print("Invalid type for phone:youtube_music:soundStatus: " .. type)
+        print("Invalid type for phone:utune_music:soundStatus: " .. type)
     end
 
-    TriggerClientEvent("phone:youtube_music:soundStatus", -1, type, musicId, data)
+    TriggerClientEvent("phone:utune_music:soundStatus", -1, type, musicId, data)
 end)
