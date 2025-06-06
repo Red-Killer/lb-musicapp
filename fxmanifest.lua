25c39180ea54237c77ed5d93b30b114deae1b89e
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2025-06-06 16:38:14 -0400

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,7 +3,7 @@ game "gta5"
 
 author "Red Killer & PrinceAlbert (Popcorn RP)"
 description "Play your favourite music through lb-phone"
-version "1.0.0"
+version "2.0.0"
 
 client_script "client.lua"
 server_script "server.lua"
