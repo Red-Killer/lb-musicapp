bc36912319bcf29164ccb2e8aed710054f0fe1dd
Popcorn RP - PrinceAlbert <85725579+alberttheprince@users.noreply.github.com>
2025-08-20 00:36:11 -0400

    Update fxmanifest.lua
    added config to list

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,6 +5,7 @@ author "Red Killer & PrinceAlbert (Popcorn RP)"
 description "Play your favourite music through lb-phone"
 version "2.0.0"
 
+shared_script "config.lua"
 client_script "client.lua"
 server_script "server.lua"
 
